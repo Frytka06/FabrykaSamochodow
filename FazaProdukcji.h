cd9b67364db12dfb71ac844c41b1365814ac171b
Fryderyk Forys <fryderykforys2006@gmail.com>
2025-06-17 14:09:00 +0200

    Enum Fazaprodukcji.h dodany

---
+++ FazaProdukcji.h
@@ -0,0 +1,15 @@
+//
+// Created by Fryderyk on 17.06.2025.
+//
+
+#ifndef FAZAPRODUKCJI_H
+#define FAZAPRODUKCJI_H
+
+enum class FazaProdukcji {
+    MONTAZ,
+    MALOWANIE,
+    KONTROLA_JAKOSCI,
+    SKONCZONE
+};
+
+#endif //FAZAPRODUKCJI_H
